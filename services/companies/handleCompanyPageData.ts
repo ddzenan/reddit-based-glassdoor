8a57326ab4c6033c0fba588f4ab6f2452cc21aa4
ddzenan <dzenan.devedzic@gmail.com>
2024-11-13 03:38:48 +0100

    Implemented handleCompanyPageData function

---
+++ services/companies/handleCompanyPageData.ts
@@ -0,0 +1,57 @@
+import {
+  getDocumentsByField,
+  updateDocumentAndAddToSubcollection,
+} from "@/lib/firebaseAdmin/dataServices";
+import { fetchPostsAndComments } from "../reddit";
+import { analyzeRedditPosts } from "../openai";
+import { RedditPostWithComments, Company } from "@/types";
+import { countSentiments } from "@/utils/helpers";
+import { ANALYSIS_TYPES } from "@/utils/constants";
+
+/**
+ * Retrieves and prepares company data for a company page based on the provided slug.
+ * If the company has no summary, the function fetches Reddit posts, analyzes sentiments,
+ * generates a summary, and updates Firestore with new data.
+ *
+ * @param slug - Unique identifier (slug) for the company.
+ * @returns Returns the company data, including summary and sentiment counts.
+ *
+ * @throws Throws error if no company matches the provided slug or if data fetching fails.
+ */
+export async function handleCompanyPageData(slug: string): Promise<Company> {
+  const companies = await getDocumentsByField("companies", "slug", slug);
+  if (!companies || companies.length === 0)
+    throw new Error(`There is no company with slug ${slug}.`);
+  let company = companies[0] as Company;
+  const { id: companyId, name: companyName, summary: companySummary } = company;
+  if (!companySummary) {
+    const redditPosts = await fetchPostsAndComments({
+      searchTerm: companyName,
+    });
+    const [summary, redditPostsWithSentiments] = await Promise.all([
+      analyzeRedditPosts(
+        ANALYSIS_TYPES.companySummary,
+        redditPosts,
+        companyName
+      ) as Promise<string>,
+      analyzeRedditPosts(
+        ANALYSIS_TYPES.sentiments,
+        redditPosts,
+        companyName
+      ) as Promise<RedditPostWithComments[]>,
+    ]);
+    const sentimentCounts = countSentiments(redditPostsWithSentiments);
+    company = {
+      ...company,
+      ...sentimentCounts,
+      summary,
+    };
+    await updateDocumentAndAddToSubcollection(
+      `companies/${companyId}`,
+      company,
+      "redditPosts",
+      redditPostsWithSentiments
+    );
+  }
+  return company;
+}
