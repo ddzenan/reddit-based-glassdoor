a4a599438c41c2362e1e8e39d722a0359bbe0880
ddzenan <dzenan.devedzic@gmail.com>
2024-11-13 23:34:49 +0100

    Implemented filterComments function

--- services/reddit/fetchPostsAndComments.ts
+++ services/reddit/fetchPostsAndComments.ts
@@ -9,6 +9,9 @@ import {
 const DEFAULT_SUBREDDIT = "cscareerquestions";
 const DEFAULT_POSTS_TIME_PERIOD = "year";
 const DEFAULT_POSTS_SORT_OPTION = "new";
+const COMMENTS_AMOUNT = 5;
+
+const DELETED_COMMENTS_KEYWORS = ["[deleted]", "[removed]"];
 
 /**
  * Fetches posts and their comments from a specified subreddit.
@@ -38,7 +41,7 @@ export async function fetchPostsAndComments({
   return await Promise.all(
     posts.map(async (post) => {
       const fetchedComments = await fetchCommentsForPost(post);
-      const comments = fetchedComments.map((comment: Comment) => comment.body);
+      const filteredComments = filterComments(fetchedComments, COMMENTS_AMOUNT);
       return {
         id: post.id,
         created: post.created_utc,
@@ -48,8 +51,23 @@ export async function fetchPostsAndComments({
         url: post.url,
         title: post.title,
         text: post.selftext,
-        comments: comments,
+        comments: filteredComments,
       };
     })
   );
+}
+
+/**
+ * Filters a list of Reddit comments to extract the text content (body) and removes deleted comments.
+ * It returns the specified number of comments, excluding any that have been deleted.
+ *
+ * @param comments - An array of Reddit comment objects to be filtered.
+ * @param amount - The maximum number of comments to return.
+ * @returns A list of strings representing the bodies of the filtered comments, up to the specified amount.
+ */
+function filterComments(comments: Comment[], amount: number): string[] {
+  return comments
+    .map((comment) => comment.body)
+    .filter((comment) => comment && !DELETED_COMMENTS_KEYWORS.includes(comment))
+    .slice(0, amount);
 }
