493a211fed2ec9d499ee7314bc0eae073db1851f
ddzenan <dzenan.devedzic@gmail.com>
2024-11-22 16:07:19 +0100

    Implemented function for retrieving list of companies

---
+++ services/companies/handleCompaniesPageData.ts
@@ -0,0 +1,26 @@
+import { fetchAllDocuments } from "@/lib/firebaseAdmin/dataServices";
+import { fetchClearbitLogo } from "@/lib/clearbit/dataServices";
+import { CompanyCardProps } from "@/types";
+
+/**
+ * Handles fetching and processing data for the /companies page.
+ *
+ * @returns A promise that resolves to an array of company objects that contains name, slug and logo.
+ */
+export async function handleCompaniesPageData(): Promise<CompanyCardProps[]> {
+  const companies = await fetchAllDocuments("companies", [
+    "name",
+    "slug",
+    "website",
+  ]);
+  const companiesWithLogos = await Promise.all(
+    companies.map(async (company) => {
+      if (!company.website) return company;
+      const logo = await fetchClearbitLogo(company.website);
+      delete company.website;
+      if (logo) return { ...company, logo };
+      return company;
+    })
+  );
+  return companiesWithLogos;
+}
