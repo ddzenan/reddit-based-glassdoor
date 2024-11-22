704722b573b81d0a1fc4e4ae88265309f2dcc522
ddzenan <dzenan.devedzic@gmail.com>
2024-11-22 03:50:54 +0100

    Implemented functionality for text expansion

---
+++ components/shared/ExpandableTextWithTitle.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import Card from "@/components/shared/Card";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { truncateText } from "@/utils/helpers";
+
+const SHORT_TEXT_MAX_LENGTH = 600;
+
+/**
+ * Props for the `ExpandableTextWithTitle` component.
+ *
+ * @property {string} [title] - The optional title displayed at the top of the card.
+ * @property {string} text - The main content text to be displayed, which can be toggled between a truncated and full view.
+ */
+type ExpandableTextWithTitleProps = {
+  title?: string;
+  text: string;
+};
+
+/**
+ * A React component that renders a card with an optional title and expandable text content.
+ * The text is initially displayed in a truncated form, but can be expanded or collapsed
+ * using a toggle button.
+ *
+ * @param {ExpandableTextWithTitleProps} props - The properties for the component, including an optional title and the main content text.
+ * @returns {JSX.Element} A JSX element that displays a card with expandable text functionality.
+ */
+export default function ExpandableTextWithTitle({
+  title,
+  text,
+}: ExpandableTextWithTitleProps) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleText = () => setIsExpanded((prev) => !prev);
+
+  const displayedText = isExpanded
+    ? text
+    : truncateText(text, SHORT_TEXT_MAX_LENGTH);
+
+  return (
+    <Card title={title}>
+      <p className="text-sm text-muted-foreground">{displayedText}</p>
+      <Button
+        variant="outline"
+        className="mt-4 text-xs font-normal"
+        onClick={toggleText}
+      >
+        {isExpanded ? "Show less" : "Show more"}
+      </Button>
+    </Card>
+  );
+}
