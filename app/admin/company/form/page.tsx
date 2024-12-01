24035ca5942d84be4668b7828922a8db5048b4bf
ddzenan <dzenan.devedzic@gmail.com>
2024-12-01 04:29:40 +0100

    Implemented form for adding or editing an company

---
+++ app/admin/company/form/page.tsx
@@ -0,0 +1,29 @@
+import CompanyForm from "@/components/admin/company/form/CompanyForm";
+
+/**
+ * Props for the `CompanyFormPage` component.
+ *
+ * @property {Object} searchParams - The search parameters for the page, including the company ID.
+ * @property {string} searchParams.id - The ID of the company to be edited.
+ */
+type CompanyFormPageProps = {
+  searchParams: {
+    id: string;
+  };
+};
+
+/**
+ * A Next.js page component that renders a form for adding or editing a company.
+ *
+ * @param {CompanyFormPageProps} props - The properties passed to the `CompanyFormPage` component.
+ * @returns {Promise<JSX.Element>} A JSX element that renders the company form.
+ */
+export default async function CompanyFormPage(props: CompanyFormPageProps) {
+  const { searchParams } = await props;
+  const { id } = await searchParams;
+  return (
+    <div className="max-w-screen-sm mx-auto px-2 py-8 sm:py-16">
+      <CompanyForm companyId={id} />
+    </div>
+  );
+}
---
+++ components/admin/company/form/CompanyForm.tsx
@@ -0,0 +1,178 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CompanySchema } from "@/schemas";
+import { Company } from "@/types";
+import { REVENUE_OPTIONS } from "@/utils/constants";
+import {
+  fetchCompany,
+  saveCompany,
+} from "@/services/companies/basicCompanyActions";
+import {
+  generateSlug,
+  handleInputNullableFieldChange,
+  transformToNull,
+  transformToUndefined,
+} from "@/utils/helpers";
+import { useSuccessToast, useErrorToast } from "@/hooks/useToasts";
+import { Form } from "@/components/ui/form";
+import FormFieldWrapper from "@/components/shared/FormFieldWrapper";
+import { Button } from "@/components/ui/button";
+import BasicFormSkeleton from "@/components/shared/BasicFormSkeleton";
+
+const FIELDS = [
+  "name",
+  "slug",
+  "website",
+  "yearFounded",
+  "numberOfEmployees",
+  "estimatedRevenue",
+] as (keyof Company)[];
+
+const NUMBER_OF_FIELDS = FIELDS.length;
+
+const REVENUE_OPTIONS_FOR_SELECT = REVENUE_OPTIONS.map((option) => ({
+  key: option,
+  value: option,
+}));
+
+/**
+ * Props for the `CompanyForm` component.
+ *
+ * @property {string} [companyId] - The optional ID of the company to fetch and edit.
+ * If not provided, the form will be initialized for creating a new company.
+ */
+type CompanyFormProps = {
+  companyId?: string;
+};
+
+/**
+ * A React component that renders a form for creating or editing a company.
+ * The form supports validation, data fetching, and submission, while handling
+ * missing values by transforming them to `null`.
+ *
+ * @param {CompanyFormProps} props - The properties for the component, including an optional `companyId` for fetching and editing an existing company.
+ * @returns {JSX.Element} A JSX element that renders the company form, including fields for name, slug, website, year founded, number of employees, and estimated revenue.
+ */
+export default function CompanyForm({ companyId }: CompanyFormProps) {
+  const [isFormLoading, setIsFormLoading] = useState<boolean>(true);
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const [areDataFetched, setAreDataFetched] = useState<boolean>(false);
+  const showSuccessToast = useSuccessToast();
+  const showErrorToast = useErrorToast();
+  const router = useRouter();
+  const form = useForm<z.infer<typeof CompanySchema>>({
+    resolver: zodResolver(CompanySchema),
+    defaultValues: {
+      name: "",
+      slug: "",
+      website: null,
+      yearFounded: null,
+      numberOfEmployees: null,
+      estimatedRevenue: null,
+    },
+  });
+  const { control, handleSubmit, setValue, reset } = form;
+
+  useEffect(() => {
+    async function getCompanyData() {
+      try {
+        if (companyId && !areDataFetched) {
+          setAreDataFetched(true);
+          const company = await fetchCompany(companyId, FIELDS);
+          if (!company) return;
+          const transformedData = transformToNull(company, FIELDS);
+          if (company) reset(transformedData);
+        }
+      } catch (error) {
+        showErrorToast();
+      } finally {
+        setIsFormLoading(false);
+      }
+    }
+    getCompanyData();
+  }, [companyId, reset, showErrorToast]);
+
+  function handleNameChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const name = e.target.value;
+    setValue("name", name);
+    setValue("slug", generateSlug(name));
+  }
+
+  async function onSubmit(data: z.infer<typeof CompanySchema>) {
+    setIsSubmitting(true);
+    try {
+      const transformedData = transformToUndefined(data) as Company;
+      await saveCompany(transformedData, companyId);
+      showSuccessToast(
+        `Successfully ${companyId ? "updated" : "created"} company!`
+      );
+      if (companyId) return;
+      router.push("/admin/company/form");
+      reset();
+    } catch (error) {
+      showErrorToast();
+    } finally {
+      setIsSubmitting(false);
+    }
+  }
+
+  return (
+    <div>
+      <div className="font-semibold text-2xl mb-16">Add Company</div>
+      {isFormLoading ? (
+        <BasicFormSkeleton numberOfFields={NUMBER_OF_FIELDS} />
+      ) : (
+        <Form {...form}>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+            <FormFieldWrapper
+              name="name"
+              control={control}
+              label="Name"
+              onChange={handleNameChange}
+            />
+            <FormFieldWrapper name="slug" control={control} label="Slug" />
+            <FormFieldWrapper
+              name="website"
+              control={control}
+              label="Website"
+              onChange={handleInputNullableFieldChange()}
+            />
+            <FormFieldWrapper
+              name="yearFounded"
+              control={control}
+              label="Year Founded"
+              onChange={handleInputNullableFieldChange((v) => parseInt(v, 10))}
+              type="number"
+            />
+            <FormFieldWrapper
+              name="numberOfEmployees"
+              control={control}
+              label="Number of Employees"
+              onChange={handleInputNullableFieldChange((v) => parseInt(v, 10))}
+              type="number"
+            />
+            <FormFieldWrapper
+              name="estimatedRevenue"
+              control={control}
+              label="Estimated Revenue"
+              type="select"
+              options={REVENUE_OPTIONS_FOR_SELECT}
+            />
+            <Button type="submit" disabled={isSubmitting}>
+              {isSubmitting
+                ? "Please wait..."
+                : companyId
+                ? "Update Company"
+                : "Add Company"}
+            </Button>
+          </form>
+        </Form>
+      )}
+    </div>
+  );
+}
