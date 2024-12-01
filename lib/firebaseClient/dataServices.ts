85f1b5b8dbf90aeb04d63a71ffb4197418cc9baa
ddzenan <dzenan.devedzic@gmail.com>
2024-12-01 17:25:23 +0100

    Implemented function for fetching documents by field value

--- lib/firebaseClient/dataServices.ts
+++ lib/firebaseClient/dataServices.ts
@@ -4,6 +4,10 @@ import {
   setDoc,
   collection,
   deleteField,
+  getDocs,
+  query,
+  where,
+  QueryConstraint,
 } from "firebase/firestore";
 import { firestore } from "./firebaseClient";
 
@@ -79,4 +83,40 @@ export async function saveDocument({
   const docRef = doc(firestore, collectionPath, documentId);
   await setDoc(docRef, preparedData, { merge });
   return documentId;
+}
+
+/**
+ * Fetches documents from a Firestore collection based on a specific field and its value.
+ *
+ * @param collectionPath - The path to the Firestore collection to query.
+ * @param field - The name of the field to filter documents by.
+ * @param value - The value of the field to match documents against.
+ * @param selectFields - An optional array of fields to select from the document.
+ * @returns A promise that resolves to an array of objects that match the query.
+ */
+export async function searchDocumentsByField(
+  collectionPath: string,
+  field: string,
+  value: any,
+  selectFields?: string[]
+): Promise<Record<string, any>[]> {
+  const collectionRef = collection(firestore, collectionPath);
+  const constraints: QueryConstraint[] = [where(field, "==", value)];
+  const queryRef = query(collectionRef, ...constraints);
+
+  const snapshot = await getDocs(queryRef);
+
+  if (snapshot.empty) return [];
+
+  return snapshot.docs.map((doc) => {
+    const data = doc.data();
+    if (selectFields && selectFields.length > 0) {
+      const filteredData: Record<string, any> = {};
+      selectFields.forEach((field) => {
+        if (field in data) filteredData[field] = data[field];
+      });
+      return filteredData;
+    }
+    return data;
+  });
 }
