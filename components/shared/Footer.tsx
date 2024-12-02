921bf9cb949c5696efd7a9023775aefbb31d672f
ddzenan <dzenan.devedzic@gmail.com>
2024-12-02 22:49:03 +0100

    Changed text contet and style

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -5,14 +5,13 @@
  */
 export default function Footer() {
   return (
-    <footer className="text-foreground py-8 border-t border-gray-800">
+    <footer className="text-md text-muted-foreground py-8 border-t border-gray-800">
       <div className="max-w-screen-sm mx-auto">
         <p>
           This application analyzes data from Reddit to provide insights into
           tech companies from the perspective of employees and candidates, using
           AI for data processing.
         </p>
-        <p>Built with love and technology. ❤️</p>
       </div>
     </footer>
   );
