55ea8856ff617df65b52f9b37a00229e7a5e27e0
ddzenan <dzenan.devedzic@gmail.com>
2024-12-05 01:04:00 +0100

    Defined return type

--- hooks/useCompanyData.ts
+++ hooks/useCompanyData.ts
@@ -3,17 +3,32 @@ import { fetchCompany } from "@/services/companies/basicCompanyActions";
 import { Company, CompanyKey } from "@/types";
 
 /**
+ * Represents the return type of the `useCompanyData` hook.
+ *
+ * This type encapsulates the state and data fetched by the hook for a company.
+ *
+ * @property {Partial<Company>} data - The fetched company data, containing only the requested fields. Initially an empty object.
+ * @property {boolean} isLoading - Indicates whether the data fetch is currently in progress.
+ * @property {boolean} isError - Indicates whether an error occurred during the data fetch.
+ */
+type UseCompanyDataReturn = {
+  data: Partial<Company>;
+  isLoading: boolean;
+  isError: boolean;
+};
+
+/**
  * This hook retrieves company data from an API based on the provided company ID and specified fields.
  * It manages loading state, ensures data is fetched only once per company ID, and handles errors gracefully.
  *
  * @param companyId - The ID of the company to fetch. If not provided, no data will be fetched.
  * @param fields - The specific fields of the company to include in the fetch. Defaults to an empty array.
- * @returns An object containing:
- * - `data`: The fetched company data. Initially an empty object, updated once the data is successfully fetched.
- * - `isLoading`: Boolean indicating whether the data is currently being loaded.
- * - `isError`: Boolean indicating whether an error occurred during the data fetch.
+ * @returns The state of the fetch operation, including the fetched data, loading state, and error state.
  */
-export function useCompanyData(companyId?: string, fields?: CompanyKey[]) {
+export function useCompanyData(
+  companyId?: string,
+  fields?: CompanyKey[]
+): UseCompanyDataReturn {
   const [data, setData] = useState<Partial<Company>>({});
   const [isLoading, setIsLoading] = useState(!!companyId);
   const [isError, setIsError] = useState(false);
