83039bedf1712b57e02020617a370583020eb930
ddzenan <dzenan.devedzic@gmail.com>
2024-12-16 02:04:15 +0100

    Removed function for retrieving companies

--- app/companies/page.tsx
+++ app/companies/page.tsx
@@ -1,33 +1,17 @@
-import { handleCompaniesPageData } from "@/services/companies/handleCompaniesPageData";
+//import { handleCompaniesPageData } from "@/services/companies/handleCompaniesPageData";
 import CompanyList from "@/components/companies/CompanyList";
 import { CompanyBasicInfoWithLogo } from "@/types";
-
-/**
- * Props for the `CompaniesPage` component.
- */
-type CompaniesPageProps = {
-  companies: CompanyBasicInfoWithLogo[];
-};
 
 /**
  * A Next.js page component that renders a list of companies.
  *
  * @returns A JSX element that renders the companies page.
  */
-export default async function CompaniesPage({ companies }: CompaniesPageProps) {
+export default async function CompaniesPage() {
+  const companies: CompanyBasicInfoWithLogo[] = [];
   return (
     <div className="max-w-screen-md mx-auto px-2 py-8 sm:py-16">
       <CompanyList companies={companies} />
     </div>
   );
-}
-
-export async function getServerSideProps() {
-  const companies = await handleCompaniesPageData();
-
-  return {
-    props: {
-      companies,
-    },
-  };
 }
