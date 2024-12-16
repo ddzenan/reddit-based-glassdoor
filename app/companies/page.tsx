4efcef3fb42f406469065a0a2123d3b87efa22c2
ddzenan <dzenan.devedzic@gmail.com>
2024-12-16 02:22:04 +0100

    Added handleCompaniesPageData function call

--- app/companies/page.tsx
+++ app/companies/page.tsx
@@ -1,6 +1,5 @@
-//import { handleCompaniesPageData } from "@/services/companies/handleCompaniesPageData";
+import { handleCompaniesPageData } from "@/services/companies/handleCompaniesPageData";
 import CompanyList from "@/components/companies/CompanyList";
-import { CompanyBasicInfoWithLogo } from "@/types";
 
 /**
  * A Next.js page component that renders a list of companies.
@@ -8,7 +7,7 @@ import { CompanyBasicInfoWithLogo } from "@/types";
  * @returns A JSX element that renders the companies page.
  */
 export default async function CompaniesPage() {
-  const companies: CompanyBasicInfoWithLogo[] = [];
+  const companies = await handleCompaniesPageData();
   return (
     <div className="max-w-screen-md mx-auto px-2 py-8 sm:py-16">
       <CompanyList companies={companies} />
